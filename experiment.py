6480cbd8443bcb8bc35f6cdef3332a60591af443
GoWoon Kim <gowoonkim@GoWoons-MacBook-Pro.local>
2022-06-03 03:23:31 +0100

    fix: changes to experiment script

--- experiment.py
+++ experiment.py
@@ -4,10 +4,8 @@ import os, subprocess
 
 def main():
     for config in os.listdir("configs"):
-        for i in range(1, 6): 
-            print(f"lein run configs/{config} {i}")
-        # for i in range(0, 1): 
-        #     os.system(f"lein run {config} {i}")
+        for i in range(1, 151): 
+            os.system(f"lein run configs/{config} {i}")
 
 if __name__ == "__main__":
     main()
